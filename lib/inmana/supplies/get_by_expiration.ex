8cd0b562275f0c8b2e6a73bd430f7fce52a34de1
José Airton Neto <joseairtonneto@users.noreply.github.com>
2021-05-03 21:12:19 -0300

    Update get_by_expiration.ex

--- lib/inmana/supplies/get_by_expiration.ex
+++ lib/inmana/supplies/get_by_expiration.ex
@@ -9,12 +9,14 @@ defmodule Inmana.Supplies.GetByExpiration do
     end_of_week = Date.end_of_week(today)
 
     query =
-      from supply in Supply,
+      from(supply in Supply,
         where:
           supply.expiration_date >= ^beginning_of_week and supply.expiration_date <= ^end_of_week,
-          preload: [:restaurant]
+        preload: [:restaurant]
+      )
+
     query
-    |>Repo.all()
-    |>Enum.group_by(fn %Supply{restaurant: %Restaurant{email: email}} -> email end)
+    |> Repo.all()
+    |> Enum.group_by(fn %Supply{restaurant: %Restaurant{email: email}} -> email end)
   end
 end
